eb00ff7b65e6bbd505af7d374786023d6f300b01
dyzhin <34241406+dyzhin@users.noreply.github.com>
2017-12-07 15:33:54 +0300

    Add files via upload

---
+++ store.py
@@ -0,0 +1,58 @@
+import argparse, sys, os, json, tempfile
+
+parser = argparse.ArgumentParser(description='Store.')
+parser.add_argument("--key")
+parser.add_argument("--val")
+
+params = vars(parser.parse_args())
+
+#print(params)
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+
+if not os.path.isfile(storage_path):
+    #print("Пидарасы, где файл?!")
+    dict_is_empty = True
+elif not os.path.getsize(storage_path):
+    #print("Вы мне пытаетесь подсунуть пустое файло!")
+    dict_is_empty = True
+else:
+    dict_is_empty = False
+
+if params["val"]:
+    #print("Бля, это добавление нового слова!")
+    #print(dict_is_empty)
+
+    if dict_is_empty:
+        jdict = {}
+    else:
+        with open(storage_path, 'r') as f:
+            jdict = json.load(f)
+
+    if params["key"] in jdict:
+        jdict[params["key"]].append(params["val"])
+    else:
+        jdict[params["key"]] = [params["val"]]
+    with open(storage_path, 'w') as f:
+        json.dump(jdict,  f)
+else:
+    #print("Бля, это запрос к базе!")
+    if dict_is_empty:
+        #print("Негде, блядь, искать, словаря-то нет!")
+        print(None)
+    else:
+        with open(storage_path, 'r') as f:
+            jdict = json.load(f)
+            #print(jdict)
+            #print(params["key"])
+            #print(params["val"])
+            #print(params["key"] in jdict)
+            if params["key"] in jdict:
+                testvar = jdict[params["key"]]
+                result = ""
+                for i in range(len(testvar)):
+                    result = result + testvar[i] + ", "*(i != len(testvar)-1)
+                print(result)
+            else:
+                #print("Искали, но не нашли. Подите в хуй.")
+                print(None)
